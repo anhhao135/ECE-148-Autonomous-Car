b67a524f4042b060c712378ef50575523840ac29
Hao Le <anhhao135@gmail.com>
2020-12-01 03:14:08 -0800

    Update steering_client.py

--- scripts/steering_client.py
+++ scripts/steering_client.py
@@ -3,12 +3,16 @@ import rospy
 from std_msgs.msg import Float32
 from adafruit_servokit import ServoKit
 
+#more documentation at https://learn.adafruit.com/16-channel-pwm-servo-driver/python-circuitpython
+# kit.servo[1].set_pulse_width_range(1000, 2000) ; use this to set the calibration min and max pwms, but by default its pretty spot on for our car
+
 
 kit = ServoKit(channels = 16)
 
 
-def callback(data):
-    rospy.loginfo(data.data)
+def callback(data): #called everytime topic is updated
+    rospy.loginfo(data.data) # just for debug
+    kit.servo[1].angle = 180 * data.data #data.data is a float from 0 - 1; it is a normalized scale of the full 180 range of the servo
 
 
 def listener():
@@ -17,12 +21,10 @@ def listener():
     # anonymous=True flag means that rospy will choose a unique
     # name for our 'listener' node so that multiple listeners can
     # run simultaneously.
-    rospy.init_node('steering_client', anonymous=False)
-
+    rospy.init_node('steering_client', anonymous=True)
     rospy.Subscriber("steering", Float32, callback)
+    rospy.spin()   # spin() simply keeps python from exiting until this node is stopped
 
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
 
 
 if __name__ == '__main__':
