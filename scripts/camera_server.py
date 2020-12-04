4b0f07007c458f227b52ce0e90798b37f61e2848
anhhao135 <anhhao135@gmail.com>
2020-12-03 19:55:34 -0800

    Update camera_server.py

--- scripts/camera_server.py
+++ scripts/camera_server.py
@@ -9,29 +9,13 @@ cap = cv2.VideoCapture(0)
 
 camera_frequency = 20 # 20Hz
 
-
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Display the resulting frame
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
-
 def talker():
     pub = rospy.Publisher('camera_rgb', Image, queue_size=10)
     rospy.init_node('camera_server', anonymous=True)
     rate = rospy.Rate(camera_frequency)
     while not rospy.is_shutdown():
         ret, frame = cap.read()
+        cv2.imshow('frame', frame)
         height = frame.shape[0]
         width = frame.shape[1]
         flattened_rgb = frame.flatten()
