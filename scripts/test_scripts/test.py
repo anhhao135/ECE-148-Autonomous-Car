99b29251ff6a5c53359077f397d3a9f5e063316f
Hao Le <h3le@ucsd.edu>
2020-12-06 10:52:37 +0000

    Update test.py

--- scripts/test_scripts/test.py
+++ scripts/test_scripts/test.py
@@ -1,80 +1,77 @@
 import numpy as np
 import cv2
-
-cap = cv2.VideoCapture(0)
-
-start_height = 325
-crop_height = 275
+from simple_pid import PID
 
 
-p = 0.6
+pid = PID(1, 0.1, 0.05, setpoint=0)
+pid.output_limits = (-1, 1)
 
-relative_offset = 0
+cap = cv2.VideoCapture("test.mov")
+
+
+crop_height = 300
+
 print("hello")
 
-while(True):
-	# Capture frame-by-frame
-	ret, frame = cap.read()
-
-
-	width = len(frame[1])
-	height = len(frame[0])
-
-	frame = cv2.GaussianBlur(frame, (5,5), 0)
+while (True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
 
 
 
-	frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    width = len(frame[1])
+    height = len(frame[0]) - crop_height
 
-	'''
-	
+    frame = frame[crop_height:height, 0:width]
+    frame_color = frame
 
+    frame = cv2.GaussianBlur(frame, (5, 5), 0)
 
-	frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-
-	frame = cv2.inRange(frame, (10, 0,100), (30,60,200))
-
-
-
+    '''
 
 
 
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
 
-	'''
-
-	
-
-	ret, frame = cv2.threshold(frame, 140, 255, cv2.THRESH_BINARY_INV)
-
-	frame = np.invert(frame)
+    frame = cv2.inRange(frame, (10, 0,100), (30,60,200))
 
 
 
-	try:
 
-		M = cv2.moments(frame)
 
-		# calculate x,y coordinate of center
-		cX = int(M["m10"] / M["m00"])
-		cY = int(M["m01"] / M["m00"])
-		print(cX/width)
 
-		cX = cX + relative_offset
 
-		# put text and highlight the center
-		cv2.circle(frame, (cX, cY), 5, (0, 255, 255), -1)
-	except:
-		pass
 
-	# Our operations on the frame come here
+    '''
 
-	# Display the resulting frame
-	cv2.imshow("cv_image", frame)
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
+    ret, frame = cv2.threshold(frame, 210, 255, cv2.THRESH_BINARY_INV)
 
+    frame = np.invert(frame)
+
+    try:
+
+        M = cv2.moments(frame)
+
+        # calculate x,y coordinate of center
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+
+        # put text and highlight the center
+        cv2.circle(frame_color, (cX, cY), 5, (0, 255, 255), -1)
+    except:
+        pass
+
+    # Our operations on the frame come here
+    #print(0.5 - cX / width)
+    print(pid(0.5 - cX / width))
+    # Display the resulting frame
+    cv2.imshow("cv_image", frame_color)
+    cv2.imshow("thresh", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 # When everything done, release the capture
 cap.release()
