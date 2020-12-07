28b9a95d9b0b0b6edecb6a183dc731f14465f113
Dominic Nightingale <djnighti@ucsd.edu>
2020-12-07 01:20:46 +0000

    Revert "Updated to read in the throttle calibrated values. Also some new control logic..."
    This reverts commit d1dab798e2374572bf57ae1e0ab5d675a97d356c

--- scripts/throttle_client.py
+++ scripts/throttle_client.py
@@ -2,8 +2,6 @@
 import rospy
 from std_msgs.msg import Float32
 from adafruit_servokit import ServoKit
-import json
-import os
 
 THROTTLE_NODE_NAME = 'throttle_client'
 THROTTLE_TOPIC_NAME = 'throttle'
@@ -17,18 +15,11 @@ kit = ServoKit(channels=16)
 throttle_scale = 0.2  # scale down sensitive throttle
 
 
-def callback(data): #called everytime topic is updated
-    rospy.loginfo(data.data) # just for debug
-    normalized_throttle = data.data
-    if normalized_throttle < 0:
-        throttle_delta = 0 #value currently set to make the car go into neutral when the car wants to break. Replace with this to mess with the "reverse throttle":
-                           # -1 * (reverse - neutral_throttle) * normalized_throttle  # maps the left turn value from 0 to -1 to the max left value allowed for the kit.servo object
-    elif normalized_throttle == 0:
-        throttle_delta = 0
-    else:
-        throttle_delta = (max_throttle - neutral_throttle) * normalized_throttle  # maps the left turn value from 0 to -1 to the max left value allowed for the kit.servo object
+def callback(data):  # called everytime topic is updated
+    rospy.loginfo(data.data)  # just for debug
+    # throttle takes values from -1 to 1; this is very sensitive so keep it below 0.2
+    kit.continuous_servo[0].throttle = data.data * throttle_scale
 
-    kit.continuous_servo[0].throttle = (neutral_throttle + throttle_delta) * throttle_scale # throttle takes values from -1 to 1; this is very sensitive so keep it below 0.2
 
 def listener():
     # In ROS, nodes are uniquely named. If two nodes with the same
@@ -40,19 +31,6 @@ def listener():
     rospy.Subscriber(THROTTLE_TOPIC_NAME, Float32, callback)
     rospy.spin()   # spin() simply keeps python from exiting until this node is stopped
 
-def calibration_values(): #used to retrieve the calibrated throttle values
-    parent_path = os.path.dirname(os.getcwd()) #this assumes the script will be run from the .../scripts directory with the json file located in a directory, one directory above
-    json_path = os.path.join(parent_path, 'json_files', 'car_config.json')
-    f = open(json_path,"r") #open the car configuration file to get the most recent steering calibration values
-    data = json.load(f)
-    #assign the throttle values to global variables to be used by the throttle client script
-    global max_throttle
-    max_throttle = data['max_throttle']
-    global neutral_throttle
-    neutral_throttle = data['neutral_throttle']
-    global reverse
-    reverse = data['reverse']
 
 if __name__ == '__main__':
-    calibration_values() #call calibration values first to load the values into the throttle client
     listener()
